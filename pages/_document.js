3dec22e5462940292475115404f9b0ca64390ce5
labhk <labh.k2003@gmail.com>
2023-11-06 11:27:00 +0530

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -1,5 +1,11 @@
 import { Html, Head, Main, NextScript } from 'next/document'
 
+export const metadata = {
+  title: 'Shubh Archi portfolio',
+  description: 'Showcasing Creative Architectural Designs',
+}
+
+
 export default function Document() {
   return (
     <Html lang="en">
